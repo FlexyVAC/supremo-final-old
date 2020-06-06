09bf47642bafc478bac42e874eab2fdea4cababb
Tunahan Flexy <43987975+FlexyVAC@users.noreply.github.com>
2020-06-06 17:45:39 +0300

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş Geldin' + username + '!');
-    member.guild.defaultChannel.send('Hoş Geldin '+ username +'');
-};
