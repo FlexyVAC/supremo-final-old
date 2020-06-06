313edc9bd2398c12c76dc0fa3547e9f7ab56a01b
Tunahan Flexy <43987975+FlexyVAC@users.noreply.github.com>
2020-06-06 17:49:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,8 +137,8 @@ client.elevation = message => {
 };
 
 
-client.on('guildMemberAdd', member => {
-   message.author.send("Supremo JailBreak Discord Sunucusuna Hoş Geldiniz! Steam Grubumuza Katılmak İçin : https://steamcommunity.com/groups/SupremoCommunity");
+client.on('guildMemberAdd', function(member){
+  member.send("Hello there !");
 });
 
 
