caeae417fca2b09a81d9fa55b2b512047d744a1b
Tunahan Flexy <43987975+FlexyVAC@users.noreply.github.com>
2020-06-15 16:58:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,40 +136,6 @@ client.elevation = message => {
   return permlvl;
 };
 
-if(command === "kill")
-{
-	let myRole = message.guild.roles.find("name", "Discord Admin[DA]");
-	if(!message.member.roles.has(myRole.id))
-	{
-		return message.reply("you pleb, you don't have the permission to use this command.")
-	}
-	if(message.mentions.users.size === 0) 
-	{
-	return message.reply("please mention a user to kill");
-	}
-	let killMember = message.guild.member(message.mentions.users.first());
-	if (!killMember)
-	{
-		return message.reply("user doesn't exist or not valid");
-	}
-	let roleMembro = message.guild.roles.find("name", "Players");
-	let roleSilenced = message.guild.roles.find("name", "Susturulmuş");
-	if(killMember.roles.has(roleMembro.id))
-	{
-		killMember.removeRole(roleMembro).then(() => 
-		{	
-			setTimeout(function()
-			{
-				if(!killMember.roles.has(roleSilenced.id))
-				{
-					killMember.addRole(roleSilenced);
-				}
-					message.reply(`${killMember.user.username} was succesfully silenced`).catch(console.error);
-				}, 5000);
-			}).catch(console.error);		
-	}
-}
-
 client.on('message', msg => {
   if (msg.content === prefix + 'profil') {
     msg.channel.send(msg.author.avatarURL);
