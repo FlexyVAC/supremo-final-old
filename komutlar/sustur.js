1af8f8caf77177d07fa6c2a8d234d878f45ab066
Tunahan Flexy <43987975+FlexyVAC@users.noreply.github.com>
2020-06-15 16:14:26 +0300

    Update sustur.js

--- komutlar/sustur.js
+++ komutlar/sustur.js
@@ -1,5 +1,27 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
+  
+  if(command === "sustur")
+{
+	let myRole = message.guild.roles.find("name", "Discord Admin[DA]");
+	if(!message.member.roles.has(myRole.id))
+	{
+		return message.reply("you pleb, you don't have the permission to use this command.")
+	}
+	if(message.mentions.users.size === 0) 
+	{
+	return message.reply("please mention a user to kill");
+	}
+	let killMember = message.guild.member(message.mentions.users.first());
+	if (!killMember)
+	{
+		return message.reply("user doesn't exist or not valid");
+	}
+	let roleMembro = message.guild.roles.find("name", "Players");
+	killMember.removeRole(roleMembro).then(member => 
+	{
+		message.reply(`${member.user.username} was succesfully removed to Membro role`).catch(console.error);
+	}).catch(console.error);
 
   if (!message.guild) {
   const ozelmesajuyari = new Discord.RichEmbed()
