dbe529270eed2f516aab53e9ca91865e85c3013b
Tunahan Fırat <43987975+FlexyVAC@users.noreply.github.com>
2021-05-04 21:51:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,7 +130,7 @@ client.elevation = message => {
   if(!message.guild) {
 	return; }
   let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("KICK_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
